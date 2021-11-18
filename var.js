f0e2a198ea0dc0882355499ce6b13e533d8db8fb
Moni  Gupta <72276446+moni310@users.noreply.github.com>
2021-11-18 18:08:27 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,45 @@
+// var a=-18/4
+// console.log(a)
+
+
+//var x=8;
+//var y=3;
+//var z=x+y;
+//console.log(z)
+
+// let x=9
+// x ="moni"
+// console.log(y)
+
+// var x=[1,2,3,4]
+// console.log(x)
+
+
+// var a;
+// var b;
+// var c;
+// console.log(a,b,c);
+// console.log(a+2);
+
+// var num1=2.2;
+// var num2=4.1;
+// console.log(num1+num2,num1*num2)
+
+
+// var firstname="Mahendra";
+// var lastName="Bathula";
+// console.log(firstname+" "+lastName);
+
+
+// var str1 = "Welcome \t to my campus";
+// var str2 = "Welcome to\n my \tcampus";
+// console.log(str2);
+
+// var firstname="moni";
+// var lastname="kumari";
+// console.log(firstname+lastname)
+
+
+// var name="'myname'";
+// var name1="'last'";
+// console.log(name+name1)
