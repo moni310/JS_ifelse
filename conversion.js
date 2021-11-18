6ff8b596c2b75515efe3797e89859c08beb1f9e7
Moni  Gupta <72276446+moni310@users.noreply.github.com>
2021-11-18 18:09:54 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,50 @@
+// from rubi
+
+// console.log(1+"4"+4+"oh"+1)
+
+// let a="3"+2;
+// console.log(a)
+
+// var a="3"+true;
+// console.log(a)
+
+// var a=undefined+3;
+// console.log(a)
+
+// var num=3+null;
+// console.log(num)
+
+// let n="388"+null;
+// console.log(n)
+
+
+// var a="14"-6;
+// console.log(a)
+
+// let a="hello"+"word";
+// console.log(a)
+
+// let a = 2+false;
+// console.log(a)
+
+// var a="5"+null;
+// console.log(a)
+
+// var a=Number(" ");
+// console.log(a)
+
+
+// console.log(typeof(String))
+
+// var a="3";
+// var b=true;
+// console.log(a-b)
+
+// var a="2";
+// var b=null;
+// console.log(a-b)
+
+// var a="6";
+// var b=undefined;
+// console.log(a-b)
+// console.log(a+b)
