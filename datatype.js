e9e075b35f54df65730d1d13f2093bdd1e04c998
Moni  Gupta <72276446+moni310@users.noreply.github.com>
2021-11-18 18:09:36 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,64 @@
+// var greet1 = "Hi Nayak!";
+// console.log(greet1) 
+
+// var a = 'I"m going to the cl"assroom'; 
+// console.log(a)
+
+
+// var c = 'We\'ll nev\er leave you.';  
+// console.log(c)
+
+// var firstname="moni";
+// console.log(firstname)
+
+// var b="Hello this is my Dell Laptop"
+// console.log(b);
+
+// var city="mumbai";
+// console.log(city)
+
+// console.log("kk"/2)
+
+// var var1=7.9;
+// console.log(var1)
+
+
+// var num=3;
+// var num1=3;
+// console.log(typeof(num==num1))
+
+// var a=null;
+// console.log(typeof(a))
+
+// var n;
+// console.log(typeof(n))
+
+
+// var dic={"name":"moni","age":19,};
+// console.log(typeof(dic))
+
+// var obj1 = { a: 5, b: 6 };
+// obj1["b"] =7;
+// console.log(obj1)
+
+// var list1=[2,4,6,8];
+// console.log(list1)
+
+// var arr1= ["kumar", 24, "Academic Associate"];
+// arr1['kumar'] ="Nayak";
+// console.log (typeof(arr1));
+
+
+// var a=9;
+// ++a
+// console.log(++a)
+
+
+// console.log(!(4>2))
+
+// var a=9;
+// var b=99;
+// console.log(a!=b)
+
+var myStr =  "I am a \"double quoted\", string inside ,\"double quotes\""
+console.log(myStr)
