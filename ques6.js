c85e6a829650bd240de65734d6ddb8dd7b02a664
Moni  Gupta <72276446+moni310@users.noreply.github.com>
2021-11-18 18:04:50 +0530

    Add files via upload

---
+++ ques6.js
@@ -0,0 +1,9 @@
+let readlineSync=require("readline-sync");
+var user=readlineSync.questionInt("enter your number  ");
+var num1=300-123;
+if (user==num1){
+    console.log("Equal")
+}
+else{
+    console.log("Not Equal")
+}
